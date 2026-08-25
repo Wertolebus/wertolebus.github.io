c78801df02789865057c5ecd82621ce64e08547a
wertolebus <dwplko@gmail.com>
2026-08-25 16:06:40 +0200

    update bg

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+// todo:
+// 1. themes (r1kken)
+
 function get_random_int(min, max) {
     const min_ceiled = Math.ceil(min);
     const max_floored = Math.floor(max);
@@ -13,9 +16,29 @@ function refresh_isaac_card() {
         12: "XII - The Hanged Man", 13: "XIII - Death", 14: "XIV - Temperance", 15: "XV - The Devil", 
         16: "XVI - The Tower", 17: "XVII - The Stars", 18: "XVIII - The Moon", 19: "XIX - The Sun",
         20: "XX - Judgement", 21: "XXI - The World"
-    }
-    document.querySelector('.taro-title').innerHTML = `[ ${card_names[card]} ]`
-    document.querySelector('#taro > img').src = `/assets/tboi/${card}.png`
+    };
+    document.querySelector('.taro-title').innerHTML = `[ ${card_names[card]} ]`;
+    document.querySelector('#taro > img').src = `/assets/tboi/${card}.png`;
 }
 
-refresh_isaac_card()
+function bg_spawn_window(x, y, w, h) {
+    let bg = document.querySelector("#background");
+    let card = document.createElement("div");
+    let close_button = document.createElement("span");
+    // <span class="ui close-button">[ x ]</span>
+    close_button.classList.add('ui', 'close-button');
+    close_button.innerHTML = `[ x ]`
+    card.classList.add('card', 'animated');
+    card.style = `left: ${x}; top: ${y}; width: ${w}; height: ${h};`;
+    card.appendChild(close_button)
+    bg.appendChild(card);
+}
+
+for (let i = 0; i < 25; i++) {
+    let w = get_random_int(200, 350);
+    let h = get_random_int(200, 350);
+    let x = get_random_int(3, window.innerWidth - w - 3);
+    let y = get_random_int(3, window.innerHeight - h - 50);
+    bg_spawn_window(`${x}px`, `${y}px`, `${w}px`, `${h}px`);
+}
+refresh_isaac_card();
